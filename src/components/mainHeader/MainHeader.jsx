74a6bb6a75a9b1ee5e4b868494e93cf12fa1a1f9
maxmay94 <maxmay94@gmail.com>
2023-03-07 22:03:45 -0500

    hover secrets

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -3,12 +3,12 @@ const MainHeader = () => {
   return (
     <div className="bg-shallow-depth-cover h-screen w-screen bg-cover sm:bg-fixed pb-40 text-slate-900 flex">
 
-      <div className="mt-96 h-fit w-fit md:w-2/3 p-3 pr-20 backdrop-hue-rotate-180 backdrop-saturate-200"> 
+      <div className="mt-96 h-fit w-fit md:w-2/3 p-3 pr-20 backdrop-hue-rotate-180 backdrop-saturate-200 hover:backdrop-contrast-150"> 
 
-        <div className="bg-red-200/30 p-3">
-          <h1 className="font-gloock md:text-8xl text-6xl">SpinCycle</h1>
+        <div className="bg-yellow-200/30 hover:bg-red-200/30 w-[80vw] p-3">
+          <h1 className="font-gloock md:text-8xl text-6xl ">SpinCycle</h1>
           <br />
-          <div className="font-vollkorn text-2xl">The smart way to manage your clothes.</div>
+          <div className="font-vollkorn text-base md:text-2xl lg:text-3xl">The smart way to manage your clothes.</div>
           <br />
         </div>
 
