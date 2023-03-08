c27f73326d1f33d97f61974eaef5d6425edd043a
maxmay94 <maxmay94@gmail.com>
2023-03-07 19:22:29 -0500

    changes for mobile

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -6,7 +6,7 @@ const MainHeader = () => {
       <div className="mt-96 h-fit w-fit md:w-2/3 p-3 pr-20 backdrop-hue-rotate-180 backdrop-saturate-200"> 
 
         <div className="bg-red-200/30 p-3">
-          <h1 className="font-gloock text-8xl">SpinCycle</h1>
+          <h1 className="font-gloock md:text-8xl text-6xl">SpinCycle</h1>
           <br />
           <div className="font-vollkorn text-2xl">Let us take care of your laundry!</div>
           <br />
--- src/components/sectionOne/SectionOne.jsx
+++ src/components/sectionOne/SectionOne.jsx
@@ -9,19 +9,19 @@ const SectionOne = () => {
         <h1 className="font-gloock text-5xl font-bold text-center ">SpinCycle</h1>
       </div>
 
-      <div className="md:col-span-3 col-start-2 col-end-8 py-10 px-20 md:px-10 lg:px-20">
+      <div className="md:col-span-3 col-start-2 col-end-8 py-10 px-10 sm:px-20 md:px-10 lg:px-20">
         <img className="max-w-80" src="../images/man-in-machine-2.jpg" alt="man in washing machine" />
       </div>
 
       <div className="md:col-span-2 col-start-2 col-span-6 my-auto">
-        <p className="font-vollkorn text-center px-10 md:px-0">Don't worry about your laundry. From         wash-and-fold to dry cleaning and everything in-between we've got you covered door-to-door.
+        <p className="font-vollkorn text-center sm:px-10 md:px-0">Don't worry about your laundry. From         wash-and-fold to dry cleaning and everything in-between we've got you covered door-to-door.
         <br />
         <br />
         Our convenient online booking system allows you to schedule your laundry pickup and delivery at your own convenience. Plus, we offer flexible delivery options, including same-day and next-day delivery, so you can have your laundry back when you need it.
         </p>
       </div>
 
-      <div className="md:col-span-3 col-start-2 col-end-8 py-10 my-auto px-20 md:px-10 lg:px-20">
+      <div className="md:col-span-3 col-start-2 col-end-8 py-10 my-auto px-10 sm:px-20 md:px-10 lg:px-20">
         <img className="max-w-80" src="../images/woman-on-washer.jpg" alt="woman on washing machine" />
       </div>
     </div>
