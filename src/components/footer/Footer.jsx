adbe3373ef5b0a877e1c6c4a95787a1d47cd3c0e
maxmay94 <maxmay94@gmail.com>
2023-03-12 17:38:01 -0400

    git push orin main

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,50 +5,51 @@ const Footer = () => {
 
   return (
     <div className="w-screen bg-slate-300 h-fit text-slate-900 font-light p-5">
-      <div className="grid sm:grid-cols-5 grid-cols-2 gap-4 text-base text-center">
+      <div className="grid sm:grid-cols-5 grid-cols-2 gap-4 text-center">
 
         <Collapsible trigger="About SpinCycle" className="hover:underline">
           <div className="text-xs">
-            <p>Our Mission and Impact</p>
-            <p>A Career With Us</p>
-            <p>Our Terms</p>
-            <p>Privacy</p>
+            <p className="hover:underline">Our Mission and Impact</p>
+            <p className="hover:underline">A Career With Us</p>
+            <p className="hover:underline">Our Terms</p>
+            <p className="hover:underline">Privacy</p>
           </div>
         </Collapsible>
 
         <Collapsible trigger="Our Solutions" className="hover:underline">
           <div className="text-xs">
-            <p>What We Do</p>
-            <p>Where We Operate</p>
-            <p>Refer A Friend Or Family</p>
+            <p className="hover:underline">What We Do</p>
+            <p className="hover:underline">Where We Operate</p>
+            <p className="hover:underline">Refer A Friend Or Family</p>
           </div>
         </Collapsible>
 
         <Collapsible trigger="Join Us" className="hover:underline">
           <div className="text-xs">
-            <p>A Career With Us</p>
-            <p>Become A Driver</p>
-            <p>Become A Service Provider</p>
+            <p className="hover:underline">A Career With Us</p>
+            <p className="hover:underline">Become A Driver</p>
+            <p className="hover:underline">Become A Service Provider</p>
           </div>
         </Collapsible>
 
         <Collapsible trigger="Questions?" className="hover:underline">
           <div className="text-xs">
-            <p>Our FAQ</p>
-            <p>Contact Us</p>
+            <p className="hover:underline">Our FAQ</p>
+            <p className="hover:underline">Contact Us</p>
           </div>
         </Collapsible>
 
         <Collapsible trigger="Contact Us" className="hover:underline">
           <div className="text-xs">
-            <p>FaceBook</p>
-            <p>Instagram</p>
-            <p>LinkedIn</p>
-            <p>Twitter</p>
-            <p>TikTok</p>
-            <p>YouTube</p>
+            <p className="hover:underline">FaceBook</p>
+            <p className="hover:underline">Instagram</p>
+            <p className="hover:underline">LinkedIn</p>
+            <p className="hover:underline">Twitter</p>
+            <p className="hover:underline">TikTok</p>
+            <p className="hover:underline">YouTube</p>
           </div>
         </Collapsible>
+        
       </div>
 
     </div>
