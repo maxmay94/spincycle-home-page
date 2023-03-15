f548dba63e0959665a5c13fb313417ae546bf2de
maxmay94 <maxmay94@gmail.com>
2023-03-15 19:15:40 -0400

    final change for tn

--- src/Pages/aCareerWithUs/ACareerWithUs.jsx
+++ src/Pages/aCareerWithUs/ACareerWithUs.jsx
@@ -20,31 +20,32 @@ const ACareerWithUs = () => {
 
       <div className="text-3xl pt-20 pb-10">A Career With Us</div>
 
-      <div className="mb-auto  text-justify  bg-slate-200">
-        <div className="grid grid-cols-1 gap-8 pb-20">
+      <div className="mb-auto text-justify  bg-slate-200">
+        <div className="grid lg:grid-cols-1 grid-cols-1 gap-8 pb-2 mx-16 lg:mx-80">
 
-          <div className="col-span-2 mx-16 sm:mx-20">
+          <div className="col-span-2 ">
             <p className=""><span className="text-4xl">Our Mission</span>: SpinCycle is here to ensure that every step of the laundry process is easy and efficient. We do this by connecting people with local businesses that provide laundry services in their area in the most convenient way possible.
             </p>
             <br />
             <p>We are here to create an ecosystem that revolves around the laundry care industry. When you work with us, you know that we have dedicated our time and energy to help local businesses do their part to contribute to their communities. We do this by helping them manage their networks through innovation and sustainability in a way that allows them to better provide their services to their customers.
             </p>
           </div>
+          
 
-          <div className="text-center grid sm:grid-cols-2 grid-cols-1 mx-8 sm:mx-20">
+          <div className="text-center grid sm:grid-cols-2 grid-cols-1 mb-20">
             <div className="sm:pb-0 pb-10">
               <h2 className="text-2xl">What you bring to the table?</h2>
-              <div className="text-left sm:ml-10 ml-6">
+              <div className="mt-2">
                 <li>A Dedication to the work you do</li>
                 <li>Innovation to our systems and processes</li>
                 <li>Leadership qualities to help us</li>
                 <li>A good aditude and a thirst for knowledge</li>
               </div>
             </div>
-            
-            <div>
+
+            <div className="">
               <h2 className="text-2xl">What we bring to the table?</h2>
-              <div className="text-left sm:ml-10 ml-6">
+              <div className="mt-2">
                 <li>A competitive compensation package for your hard work</li>
                 <li>Support to help you succeed</li>
                 <li>A pathway to grow with us</li>
