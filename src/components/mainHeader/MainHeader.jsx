1ae94c5f710b233dea1ad90e6a9d95da9942a0f2
maxmay94 <maxmay94@gmail.com>
2023-03-15 17:24:43 -0400

    stash changes

--- src/components/mainHeader/MainHeader.jsx
+++ src/components/mainHeader/MainHeader.jsx
@@ -14,7 +14,7 @@ const MainHeader = () => {
       <div className="mt-96 h-fit w-fit md:w-2/3 p-3 pr-20 backdrop-hue-rotate-180 backdrop-saturate-200 backdrop-contrast-100 hover:backdrop-contrast-150 transition duration-700"> 
         <div className="bg-yellow-200/30 hover:bg-red-200/30 w-[80vw] p-3 transition duration-700">
 
-          <h1 className="font-gloock md:text-8xl text-6xl ">SpinCycle</h1>
+          <h1 className="font-gloock md:text-8xl text-6xl">SpinCycle</h1>
           <br />
 
           <div className="font-vollkorn text-base md:text-2xl lg:text-3xl">The smart way to manage your clothes.</div>
