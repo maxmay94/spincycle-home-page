f46c0da41c9388feb6129669e65afa824debc02f
maxmay94 <maxmay94@gmail.com>
2023-03-17 12:13:08 -0400

    increase font size on become a driver

--- src/Pages/becomeADriver/BecomeADriver.jsx
+++ src/Pages/becomeADriver/BecomeADriver.jsx
@@ -16,27 +16,27 @@ const BecomeADriver = () => {
         breakpoint='lg:grid-cols-3'
       >
 
-      <div className="font-vollkorn">
-          <p className="text-4xl mb-5">
+      <div>
+          <p className="text-4xl lg:text-5xl mb-5">
             Become a driver
           </p>
 
-          <p className="text-lg">Why drive with SpinCycle?</p>
-          <div className="text-sm mx-5">
+          <p className="text-lg lg:text-xl">Why drive with SpinCycle?</p>
+          <div className="text-sm lg:text-base mx-5">
             <p className="py-2">You’re working with local businesses to help them better help their communities</p>
             <p className="py-2">Our schedule revolves around your time, not the other way around</p>
             <p className="py-2">We have an extensive support network to help you if and when you need it</p>
           </div>
 
-          <p className="text-lg mt-10">What's in it for you?</p>            
-          <div className="text-sm mx-5">
+          <p className="text-lg lg:text-xl mt-10">What's in it for you?</p>            
+          <div className="text-sm lg:text-base mx-5">
             <p className="py-2">Base pay per order - no order is too small, your money is yours to keep</p>
             <p className="py-2">Your tips - belong to you, we are completely transparent and we will never take a share of the money you worked for</p>
             <p className="py-2">Bonus - lots of chances to earn on all the work you put in</p>
           </div>
 
-          <p className="text-lg mt-10">How do I join?</p>            
-          <div className="text-sm mx-5">
+          <p className="text-lg lg:text-xl mt-10">How do I join?</p>            
+          <div className="text-sm lg:text-base mx-5">
             <p className="py-2">Are you at least 18 and older?</p>
             <p className="py-2">Do you have a car or motorcycle?</p>
             <p className="py-2">Can you pass a background check</p>
--- src/components/threeColumnsWithPictures/ThreeColumnsWithPictures.jsx
+++ src/components/threeColumnsWithPictures/ThreeColumnsWithPictures.jsx
@@ -1,14 +1,23 @@
+const pictureClass = 'max-h-[55vh] h-2/3 sm:h-96 md:h-fit sm:my-10 lg:px-16 px-3 hover:scale-110 transition duration-700'
 
 const ThreeColumnsWithPictures = (props) => {
 
   return(
     <div className={`grid ${props.breakpoint ? props.breakpoint : 'sm:grid-cols-3'} grid-cols-1 mx-5 text-center place-items-center mb-auto font-vollkorn`}>
 
-      <img className="max-h-[55vh] h-2/3 sm:h-96 md:h-fit sm:my-10 lg:px-16 px-3 hover:scale-110 transition duration-700" src={props.picture1.pic} alt={props.picture1.alt} />
+      <img 
+        className={pictureClass} 
+        src={props.picture1.pic} 
+        alt={props.picture1.alt} 
+      />
 
       {props.children}
 
-      <img className="max-h-[55vh] h-2/3 sm:h-96 md:h-fit sm:my-10 lg:px-16 px-3 hover:scale-110 transition duration-700" src={props.picture2.pic} alt={props.picture1.alt} />
+      <img 
+        className={pictureClass}
+        src={props.picture2.pic} 
+        alt={props.picture1.alt} 
+      />
     </div>
   )
 }
