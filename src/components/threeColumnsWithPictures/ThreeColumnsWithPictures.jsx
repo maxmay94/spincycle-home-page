b251717b61cbe63632fb6d3a985302729256b943
maxmay94 <maxmay94@gmail.com>
2023-03-24 14:25:25 -0400

    test become a driver page changes

--- src/components/threeColumnsWithPictures/ThreeColumnsWithPictures.jsx
+++ src/components/threeColumnsWithPictures/ThreeColumnsWithPictures.jsx
@@ -1,6 +1,6 @@
-const pictureClass = 'max-h-[550px] h-2/3 sm:h-96 md:h-fit my-10 lg:px-16 px-3 hover:scale-110 transition duration-700'
+const pictureClass = 'h-3/4 sm:h-96 md:h-2/3 lg:px-12 lg:py-20 hover:scale-110 transition duration-700'
 
-const ThreeColumnsWithPictures = (props) => {
+const ThreeColumnsWithPictures = ( props ) => {
 
   return(
     <div className={`grid ${props.breakpoint ? props.breakpoint : 'sm:grid-cols-3'} grid-cols-1 mx-5 text-center place-items-center mb-auto font-vollkorn`}>
@@ -11,12 +11,12 @@ const ThreeColumnsWithPictures = (props) => {
         alt={props.picture1.alt} 
       />
 
-        <div className="my-5">
+        <div className="lg:my-20">
           {props.children}
         </div>
 
       <img 
-        className={`${pictureClass} mb-20`}
+        className={`${pictureClass} mb-20 lg:mb-0`}
         src={props.picture2.pic} 
         alt={props.picture1.alt} 
       />
